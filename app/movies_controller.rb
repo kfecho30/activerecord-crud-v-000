1ac1655f39d970865504e9768333a3408f9367a7
Kelsey Fecho <kfecho30@gmail.com>
2018-04-03 19:25:13 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -4,9 +4,9 @@
 # def make_a_new_movie_instance    # def make_a_new_movie_instance
 #   movie = __                     #   movie = Movie.new
 # end                              # end
-
+ 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
   __
 end
