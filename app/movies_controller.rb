57da4e55cdc261b106223626d4fb5178c31646a4
Kelsey Fecho <kfecho30@gmail.com>
2018-04-03 20:21:32 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -73,7 +73,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.update_all(title: 'A Movie')
 end
 
 def can_destroy_a_single_item
@@ -86,5 +86,5 @@ def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  Movie.all.destroy
+  Movie.destroy_all
 end
